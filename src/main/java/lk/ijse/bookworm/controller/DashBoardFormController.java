3f04180c4187e20f07afafa6cba29ff30a5a8e07
pathum <ckpathumck@gmail.com>
2024-03-13 13:25:53 +0530

    create main navigation button structure

--- src/main/java/lk/ijse/bookworm/controller/DashBoardFormController.java
+++ src/main/java/lk/ijse/bookworm/controller/DashBoardFormController.java
@@ -1,4 +1,64 @@
 package lk.ijse.bookworm.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+
+import java.awt.event.ActionEvent;
+
 public class DashBoardFormController {
+    @FXML
+    private JFXButton btnBook;
+
+    @FXML
+    private JFXButton btnBorrow;
+
+    @FXML
+    private JFXButton btnBranch;
+
+    @FXML
+    private JFXButton btnHome;
+
+    @FXML
+    private JFXButton btnLogout;
+
+    @FXML
+    private JFXButton btnReturn;
+
+    @FXML
+    private JFXButton btnUser;
+
+    @FXML
+    void btnBookOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBorrowOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBranchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnHomeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogoutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReturnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUserOnAction(ActionEvent event) {
+
+    }
 }
--- src/main/java/lk/ijse/bookworm/controller/LoginFormController.java
+++ src/main/java/lk/ijse/bookworm/controller/LoginFormController.java
@@ -1,4 +1,16 @@
 package lk.ijse.bookworm.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+
+import java.awt.event.ActionEvent;
+
 public class LoginFormController {
+    @FXML
+    private JFXButton btnLogin;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
 }
