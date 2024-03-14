34486ae9c51a2f179308876b4a2d216205c3a21b
pathum <ckpathumck@gmail.com>
2024-03-14 10:06:27 +0530

    update javafx properties

--- src/main/java/lk/ijse/bookworm/controller/BookFormController.java
+++ src/main/java/lk/ijse/bookworm/controller/BookFormController.java
@@ -1,4 +1,70 @@
 package lk.ijse.bookworm.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class BookFormController {
+    @FXML
+    private JFXButton btnAdd;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    @FXML
+    private JFXComboBox<?> cmbBranch;
+
+    @FXML
+    private JFXComboBox<?> cmbGenre;
+
+    @FXML
+    private TableColumn<?, ?> colAction;
+
+    @FXML
+    private TableColumn<?, ?> colAuthor;
+
+    @FXML
+    private TableColumn<?, ?> colGenre;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colStatus;
+
+    @FXML
+    private TableColumn<?, ?> colTitle;
+
+    @FXML
+    private Label lblId;
+
+    @FXML
+    private Label lblStatus;
+
+    @FXML
+    private TableView<?> tblBook;
+
+    @FXML
+    private JFXTextField txtAuthor;
+
+    @FXML
+    private JFXTextField txtSearch;
+
+    @FXML
+    private JFXTextField txtTitle;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
 }
