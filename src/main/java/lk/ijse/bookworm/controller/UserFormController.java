104ebf9bde4cae42b8b1759e3cd7c6fa04b4656e
pathum <ckpathumck@gmail.com>
2024-03-14 11:51:54 +0530

    create UserFormController.java javafx properties

--- src/main/java/lk/ijse/bookworm/controller/UserFormController.java
+++ src/main/java/lk/ijse/bookworm/controller/UserFormController.java
@@ -1,4 +1,63 @@
 package lk.ijse.bookworm.controller;
 
-public class UserFormController {
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class UserFormController {@FXML
+private JFXButton btnAdd;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> colAction;
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colBirthDay;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private DatePicker dPickBD;
+
+    @FXML
+    private Label lblId;
+
+    @FXML
+    private TableView<?> tblUser;
+
+    @FXML
+    private JFXTextField txtAddress;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXTextField txtSearch;
+
+    @FXML
+    private JFXTextField txtTp;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
 }
