89711e88dda7f27a30f773c94e16f8088e70d433
pathum <ckpathumck@gmail.com>
2024-03-14 10:57:06 +0530

    update javafx properties

--- src/main/java/lk/ijse/bookworm/controller/BranchFormController.java
+++ src/main/java/lk/ijse/bookworm/controller/BranchFormController.java
@@ -1,5 +1,54 @@
 package lk.ijse.bookworm.controller;
 
-public class BranchFormController {
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
+public class BranchFormController {
+    @FXML
+    private JFXButton btnAdd;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> colAction;
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private Label lblId;
+
+    @FXML
+    private TableView<?> tblBranch;
+
+    @FXML
+    private JFXTextField txtAddress;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXTextField txtSearch;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
 }
